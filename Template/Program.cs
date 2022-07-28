5e9ba7055878f4251e615c216052cd56c5a0b2ae
Eduardo Cordeiro <edunascimentoxd@hotmail.com>
2022-07-27 21:45:05 -0300

    correction of application launching

--- Template/Program.cs
+++ Template/Program.cs
@@ -3,6 +3,9 @@ using Template.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("TemplateDb");
 builder.Services.AddDbContext<TemplateContext>(x => x.UseSqlServer(connectionString).EnableSensitiveDataLogging());
@@ -11,8 +14,7 @@ builder.Services.AddAuthentication();
 
 builder.Services.AddControllers();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -23,14 +25,19 @@ app.UseSwaggerUI(options =>
 });
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseExceptionHandler("/Error");
 
 
 app.UseHttpsRedirection();
